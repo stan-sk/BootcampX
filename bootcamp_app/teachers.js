bf14b4fbd6cd538a81f5cb2e19e7cdaaa7675190
Stanley Lai <stanley.lai@telus.com>
2022-12-14 14:12:13 -0800

    SQL injection

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -11,23 +11,23 @@ const pool = new Pool({
 
 // Name of Teachers That Assisted
 
-const [nodeVer, file, cohort] = process.argv;
+// const [nodeVer, file, cohort] = process.argv;
 
-pool.query(`
-SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
-FROM teachers
-JOIN assistance_requests ON teacher_id = teachers.id
-JOIN students ON student_id = students.id
-JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${cohort}'
-ORDER BY teacher;
-`)
-.then(res => {
-  res.rows.forEach(user => {
-    console.log(`${cohort} : ${user.teacher}`);
-  })
-})
-.catch(err => console.error('query error', err.stack));
+// pool.query(`
+// SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+// FROM teachers
+// JOIN assistance_requests ON teacher_id = teachers.id
+// JOIN students ON student_id = students.id
+// JOIN cohorts ON cohort_id = cohorts.id
+// WHERE cohorts.name = '${cohort}'
+// ORDER BY teacher;
+// `)
+// .then(res => {
+//   res.rows.forEach(user => {
+//     console.log(`${cohort} : ${user.teacher}`);
+//   })
+// })
+// .catch(err => console.error('query error', err.stack));
 
 // compass answer
 // pool.query(`
@@ -44,3 +44,28 @@ ORDER BY teacher;
 //     console.log(`${row.cohort}: ${row.teacher}`);
 //   })
 // });
+
+
+
+
+// SQL injection
+
+const cohortName = process.argv[2] || 'JUL02';
+const values = [`${cohortName}`];
+
+const queryString = (`
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name = $1
+ORDER BY teacher;
+`)
+
+pool.query(queryString, values)
+.then(res => {
+  res.rows.forEach(row => {
+    console.log(`${row.cohort}: ${row.teacher}`);
+  })
+});
